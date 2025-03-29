9fea305f8883fdd1ac6f7d61b20b3cc5d9066f03
ayaz-125 <ayazr425@gmail.com>
2025-03-29 14:58:08 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is the end to end machine learning project',
+    author='ayaz',
+    license='MIT',
+)
